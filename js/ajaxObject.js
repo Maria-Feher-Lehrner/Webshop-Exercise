578b4aa778a8653847016bdf0466713670585408
Maria Feher-Lehrner <maria@feher.at>
2024-05-25 19:19:12 +0200

    Implemented ajax call for product categories
    added image assets

--- js/ajaxObject.js
+++ js/ajaxObject.js
@@ -1,22 +1,88 @@
-/*class ajaxObject {
+class ajaxObject {
     constructor() {
         this.$action = $()
-        this.$categoriesOutput = $('categories-output')
-        this.$productOutput = $('products-output')
+        this.$categoriesOutput = $('#categories-output')
+        this.$productOutput = $('#products-output')
+
     }
 
     initGui() {
         this.loadCategoryData()
+        this.setupEventHandlers()
     }
 
     loadCategoryData() {
-        let url = "http://localhost/bb/Webshop/api/index.php?resource=types"
         $.ajax({
-            url: url,
-            method: 'GET',
+            url: "http://localhost/bb/Webshop/api/index.php?resource=types",
+            method: 'GET'
         })
             .done((response) => {
-                let categoriesObject = response.
+                console.log(response[0]["productType"])
+                console.log(response[0]["url"])
+                this.updateCategoriesList(response)
+            })
+            .fail(function(error) {
+                console.log(error)
             })
     }
-}*/
+
+    loadProductsData(url) {
+        $.ajax({
+            url: url,
+            method: 'GET'
+        })
+            .done((response) => {
+                console.log(response)
+                this.updateProductsList(response)
+            })
+            .fail(function(error) {
+                console.log(error)
+            })
+    }
+
+    updateCategoriesList(response) {
+        let id = 1
+        for (let item of response){
+            let listElement = $("<a>").addClass("list-group-item list-group-item-action");
+            listElement.text(item.productType)
+            console.log(item.productType)
+            listElement.attr("href", item.url)
+            listElement.attr("id", id)
+            this.$categoriesOutput.append(listElement)
+            id++
+
+            //Disables the three categories that are emtpy
+            //TODO: in eigene Methode auslagern - Version weiter unten hat unerwuenschten Effekt arbeitet nicht wie erwartet
+            //TODO: Methode evtl. dynamisch bauen, damit man nicht vorher schon wissen muss welche Kategorien leer sind?
+            if(item.productType == "mouth care" ||
+                item.productType == "shave" ||
+                item.productType == "wellness"){
+                listElement.addClass("disabled")
+                listElement.attr("disabled", "true")
+            }
+            //this.disableEmptyCategories(item)
+        }
+    }
+
+    updateProductsList(response) {
+
+    }
+    disableEmptyCategories(item) {
+        if(item.productType == "mouth care" ||
+            item.productType == "shave" ||
+            item.productType == "wellness"){
+            listElement.addClass("disabled")
+            listElement.attr("disabled", "true")
+        }
+    }
+
+    setupEventHandlers() {
+        this.$categoriesOutput.on('click', 'a', (event) => {
+            event.preventDefault();
+            let url = $(event.currentTarget).attr('data-url');
+            this.loadProductsData(url);
+        });
+    }
+
+
+}
