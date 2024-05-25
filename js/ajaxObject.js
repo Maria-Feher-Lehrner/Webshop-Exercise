21104b9d0d39ca2efefd35eeae6cb1ff2cf8a400
Maria Feher-Lehrner <maria@feher.at>
2024-05-25 15:34:56 +0200

    Implemented main.js started working on ajaxObject.js added scripts to index.html ran into trouble with ajax call - got to fix backend namespaces

---
+++ js/ajaxObject.js
@@ -0,0 +1,22 @@
+/*class ajaxObject {
+    constructor() {
+        this.$action = $()
+        this.$categoriesOutput = $('categories-output')
+        this.$productOutput = $('products-output')
+    }
+
+    initGui() {
+        this.loadCategoryData()
+    }
+
+    loadCategoryData() {
+        let url = "http://localhost/bb/Webshop/api/index.php?resource=types"
+        $.ajax({
+            url: url,
+            method: 'GET',
+        })
+            .done((response) => {
+                let categoriesObject = response.
+            })
+    }
+}*/
---
+++ js/main.js
@@ -0,0 +1,4 @@
+$( document ).ready(function(){
+    let ajaxOb = new ajaxObject()
+    ajaxOb.initGui()
+});
