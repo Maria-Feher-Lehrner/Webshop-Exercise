62c96d57b15aea368ac03ea0ec69acedbdbca7f5
Maria Feher-Lehrner <maria@feher.at>
2024-05-28 18:38:54 +0200

    Added Info for fix CORS in index.php

--- api/index.php
+++ api/index.php
@@ -2,6 +2,7 @@
 require 'vendor/autoload.php';
 
 //TODO: Header nach Loesen der CORS Probleme wieder rausnehmen!!!
-header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Origin: *");
+//ACHTUNG!: Zum Testen im Browser auch die index.html über localhost ansprechen und nicht einfach nur im Browser oeffnen!
 $app = new \Fhtechnikum\Webshop\ProductDbController();
 $app->route();
